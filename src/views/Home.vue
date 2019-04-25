7d8353104b68d9ca656bf7a71e45e53529b742d6
nickael <qaznickael@protonmail.com>
2019-04-25 16:04:20 -0300

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="home">
+    <button @click="logout">Logout</button>
     <MataMonstro/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import firebase from 'firebase'
+
 import MataMonstro from '@/components/MataMonstro.vue'
 
 export default {
   name: 'home',
   components: {
     MataMonstro
+  },
+  methods: {
+    logout: function() {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('login')
+      })
+    }
   }
 }
 </script>
